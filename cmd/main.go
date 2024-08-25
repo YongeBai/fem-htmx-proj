7eeef148ae28d527f33911a8f5b6d84a0168edb6
YongeBai <yongebai@gmail.com>
2024-08-25 17:18:35 -0400

    adding email duplicate check

--- cmd/main.go
+++ cmd/main.go
@@ -34,6 +34,15 @@ func CreateContact(name, email string) Contact {
 	}
 }
 
+func (d *Data) hasEmail(email string) bool {
+	for _, contact := range d.Contacts {
+		if contact.Email == email {
+			return true
+		}
+	}
+	return false
+}
+
 type Contacts = []Contact
 
 type Data struct {
@@ -49,23 +58,57 @@ func CreateData() Data {
 	}
 }
 
+type FormData struct {
+	Values map[string]string
+	Errors map[string]string
+}
+
+func CreateFormData() FormData {
+	return FormData {
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
+type Page struct {
+	Data Data
+	FormData FormData
+}
+
+func CreatePage() Page {
+	return Page {
+		Data: CreateData(),
+		FormData: CreateFormData(),
+	}
+}
+
 func main() {
 	e := echo.New()	
 	e.Use(middleware.Logger())
 
-	data := CreateData()	
+	page := CreatePage()
 	e.Renderer = NewTemplates()
 
 	e.GET("/", func(c echo.Context) error {		
-		return c.Render(100, "index", data)
+		return c.Render(100, "index", page)
 	})
 
 
 	e.POST("/contacts", func(c echo.Context) error {
 		name := c.FormValue("name")
 		email := c.FormValue("email")
-		data.Contacts = append(data.Contacts, CreateContact(name, email))
-		return c.Render(200, "contact-list", data)
+
+		if page.Data.hasEmail(email) {
+			formData := CreateFormData()
+			formData.Values["name"] = name
+			formData.Values["email"] = email
+			formData.Errors["email"] = "Email already exists"
+
+			return c.Render(422, "create-contact", formData)
+		}
+
+		page.Data.Contacts = append(page.Data.Contacts, CreateContact(name, email))
+		return c.Render(200, "contact-list", page.Data)
 	})
 
 
