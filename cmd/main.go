08077aeeb2a0fc1aa3ed051956d6836bb95a42e8
YongeBai <yongebai@gmail.com>
2024-08-25 18:34:28 -0400

    adding oob swap and deleting

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"io"
+	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"	
+	"github.com/labstack/echo/v4/middleware"
 )
 
 type Templates struct{
@@ -22,15 +25,19 @@ func NewTemplates() *Templates {
 	}
 }
 
+var id = 0
 type Contact struct {
 	Name string
 	Email string
+	Id int
 }
 
 func CreateContact(name, email string) Contact {
+	id++
 	return Contact{
 		Name: name,
 		Email: email,
+		Id: id,
 	}
 }
 
@@ -56,6 +63,15 @@ func CreateData() Data {
 			CreateContact("Kanye West", "kw@gmail.com"),
 		},
 	}
+}
+
+func (d *Data) indexOf(id int) int {
+	for i, contact := range d.Contacts {
+		if contact.Id == id {
+			return i
+		}
+	}
+	return -1
 }
 
 type FormData struct {
@@ -106,11 +122,31 @@ func main() {
 
 			return c.Render(422, "create-contact", formData)
 		}
-
-		page.Data.Contacts = append(page.Data.Contacts, CreateContact(name, email))
-		return c.Render(200, "contact-list", page.Data)
+		
+		contact := CreateContact(name, email)
+		page.Data.Contacts = append(page.Data.Contacts, contact)
+		c.Render(200, "create-contact", CreateFormData())
+		return c.Render(200, "oob-contact", contact)
 	})
 
+	e.DELETE("/contact/:id", func(c echo.Context) error {
+		time.Sleep(1 * time.Second)
+		idStr := c.Param("id")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			return c.String(400, "Invalid id")
+		}
+		
+		index := page.Data.indexOf(id)		
+
+		if index == -1 {
+			return c.String(404, "Contact not found")
+		}
+
+		page.Data.Contacts = append(page.Data.Contacts[:index], page.Data.Contacts[index+1:]...)
+
+		return c.NoContent(200)
+	})
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
